f677d942691c64b9892cb8ddd2567cd22dd9b338
Alex Burck <burck1@illinois.edu>
2018-09-16 23:47:36 -0500

    init gen

--- swaggerGen/cmd/gen.go
+++ swaggerGen/cmd/gen.go
@@ -1,32 +1,26 @@
 package cmd
 
 import (
-	"fmt"
+	"log"
 
 	"github.com/spf13/cobra"
 )
 
-// genCmd represents the gen command
 var genCmd = &cobra.Command{
 	Use:   "gen",
 	Short: "Generate a swagger configuration from local JSON files",
 	Long: `This command will read swagger files from the New Relic API Explorer
 and generate a single swagger.json file from them.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("gen called")
-	},
+	RunE: generateSwaggerDocs,
 }
 
 func init() {
 	rootCmd.AddCommand(genCmd)
+}
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// genCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// genCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+func generateSwaggerDocs(cmd *cobra.Command, args []string) error {
+	log.Println("Start gen")
+	cmd.DebugFlags()
+	log.Println("End gen")
+	return nil
 }
